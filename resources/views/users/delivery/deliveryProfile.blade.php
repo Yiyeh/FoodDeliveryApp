f8ab339c1b0fcdc2587528b5006854186b0a83fd
Yiyeh <yiyehtv@gmail.com>
2017-11-26 15:22:17 -0400

    se mejoro y agrego panel de recomendacion en cada index de delivery, rating con estrellas por css

--- resources/views/users/delivery/deliveryProfile.blade.php
+++ resources/views/users/delivery/deliveryProfile.blade.php
@@ -80,24 +80,54 @@
 						<p class="text-right">Puntaje {{$comment->score}}/5</p>
 						<hr>
 				    @endforeach
+				    
 				  	</div>
 				</div>
 				@else
 
 				<div class="panel panel-default">
 				  	<div class="panel-heading">
-				    	<h3 class="panel-title">Comentarios</h3>
+				    	<h3 class="panel-title">Recomentaciones</h3>
 				  	</div>
 				  	<div class="panel-body">
 					
-					<p>Este Delivery aun no tiene Recomendaciones</p>	
-				  	
+						<p>Este Delivery aun no tiene Recomendaciones</p>	
+						<br>
+										  	
 				  	</div>
 				</div>
 
 				@endif
 
-			
+				<div class="panel panel-default">
+				  	<div class="panel-heading">
+				    	<h3 class="panel-title">Dejar Recomendación</h3>
+				  	</div>
+					<div class="panel-body">
+						<form action="">
+							<div class="form-group">
+								<p class="lead">Puntaje:</p>
+								<div class="stars">
+								    <input class="star star-5" id="star-5" type="radio" name="star"/>
+								    <label class="star star-5" for="star-5"></label>
+								    <input class="star star-4" id="star-4" type="radio" name="star"/>
+								    <label class="star star-4" for="star-4"></label>
+								    <input class="star star-3" id="star-3" type="radio" name="star"/>
+								    <label class="star star-3" for="star-3"></label>
+								    <input class="star star-2" id="star-2" type="radio" name="star"/>
+								    <label class="star star-2" for="star-2"></label>
+								    <input class="star star-1" id="star-1" type="radio" name="star"/>
+								    <label class="star star-1" for="star-1"></label>
+								</div>
+								<span>
+									<input class="form-control" type="area" width="100%" placeholder="Escriba su recomendación">
+								</span><br>
+								<input class="form-control" type="submit" name="">
+							</div>	
+						</form>	
+					</div>
+				</div>
+				
 			</div> <!-- col-sm-9 -->
 		</div><!-- row -->
 	</div>
